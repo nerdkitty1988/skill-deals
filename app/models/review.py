15e0441b6cf54ce8d349fbd8602ba6ad02c9e42c
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-10-19 11:11:56 -0500

    Users and Reviews

---
+++ app/models/review.py
@@ -0,0 +1,31 @@
+from .db import db
+from sqlalchemy.sql import func
+from sqlalchemy import DateTime
+
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    reviewed_user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Integer, nullable=False)
+    author = db.relationship('User', back_populates='reviews_created')
+    reviewed_user = db.relationship('User', back_populates='reviews_recieved')
+    time_created = db.Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = db.Column(DateTime(timezone=True), onupdate=func.now())
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'authorId': self.author_id,
+            'reviewedUserId': self.reviewed_user_id,
+            'rating': self.rating,
+            'comment': self.comment,
+            'author': self.author,
+            'createdAt': self.time_created,
+            'updatedAt': self.time_updated
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,9 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.sql import func
+from sqlalchemy import DateTime
+
 
 
 class User(db.Model, UserMixin):
@@ -8,8 +11,16 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
+    private_email = db.Column(db.String(255), nullable=False, unique=True)
+    public_email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    latitude = db.Column(db.String(20), nullable=False)
+    longitude = db.Column(db.String(20), nullable=False)
+    profile_pic = db.Column(db.String(255), nullable=True)
+    reviews_recieved = db.relationship('Review', back_populates='reviewed_user', cascade="all, delete")
+    reviews_created = db.relationship('Review', back_populates='author', cascade="all, delete")
+    time_created = db.Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = db.Column(DateTime(timezone=True), onupdate=func.now())
 
     @property
     def password(self):
@@ -26,5 +37,12 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'privateEmail': self.private_email,
+            'publicEmail': self.public_email,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'profilePic': self.profile_pic,
+            'reviewsRecieved': self.reviews_recieved,
+            'createdAt': self.time_created,
+            'updatedAt': self.time_updated
         }
