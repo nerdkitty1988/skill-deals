b66506d45dbc05e5250f60bc171d793954dd7b01
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-10-22 10:28:02 -0500

    User page, offer page

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -13,7 +13,7 @@ const NavBar = () => {
 					<NavLink to="/" exact={true} className="homeNav" />
                     <NavLink to="/offers" exact={true} className="navbarButtons">Offers</NavLink>
                     <NavLink to="/requests" exact={true} className="navbarButtons">Requests</NavLink>
-                    <NavLink to="/user" exact={true} className="navbarButtons">{sessionUser.username}'s Page</NavLink>
+                    <NavLink to={`/user/${sessionUser.id}`} exact={true} className="navbarButtons">{sessionUser.username}'s Page</NavLink>
                     <form
 						id="header-search-form"
 						className="site-search-form"
