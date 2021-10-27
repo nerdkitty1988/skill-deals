391570d90fac33213629ba0719b1ba27cb87d3fd
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-10-27 11:42:51 -0500

    Small style fixes

--- react-app/src/components/SingleOffer/SingleOffer.js
+++ react-app/src/components/SingleOffer/SingleOffer.js
@@ -24,7 +24,7 @@ const SingleOffer = () => {
         <div className="wholeTradePage">
             <div className="tradeContainer">
                 <h1 className='title'>{offer?.title}</h1>
-                <NavLink className='authorLink' to={`/users/${offer?.userId}`} ><h2>by: {offer?.user.username}</h2></NavLink>
+                <NavLink className='authorLink' to={`/users/${offer?.userId}`} ><h2>by: {offer?.user.username}<div className='profilePicDiv'><img alt="profile" className="profilePicSingle" src={offer?.user.profilePic} /></div></h2></NavLink>
                 <h2 className='postedDate'>Posted on: {new Date(offer?.createdAt).toLocaleDateString()}</h2>
                 <p className='description'>Description: {offer?.description}</p>
                 <p className="singleDistance">{offerDistance ? offerDistance.toFixed() : offerDistance} miles away</p>
