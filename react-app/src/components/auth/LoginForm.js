10324de7bd292ad6632a51e02ab4e6187b6db5ac
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-10-28 16:21:26 -0500

    validations

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -53,6 +53,7 @@ const LoginForm = () => {
 					placeholder="Email"
 					value={email}
 					onChange={updateEmail}
+                    required={true}
 					className="signinInput"
 				/>
 			</div>
@@ -65,6 +66,7 @@ const LoginForm = () => {
 					type="password"
 					placeholder="Password"
 					value={password}
+                    required={true}
 					onChange={updatePassword}
 					className="signinInput"
 				/>
