04a4dd23a0727ce3dbae07248a1624f1b8ea1aa4
nerdkitty1988 <jamiarnold2284@gmail.com>
2021-11-16 12:30:42 -0600

    Request chat

--- react-app/src/components/SingleRequest/SingleRequest.js
+++ react-app/src/components/SingleRequest/SingleRequest.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
 import "./SingleRequest.css";
+import ReactModal from "react-modal";
+import Messages from "../Messages/Messages";
+import { useSelector } from "react-redux";
 
 const SingleRequest = () => {
 	const { requestId } = useParams();
 	const [request, setRequest] = useState();
 	const [distance, setDistance] = useState();
+	const [showChat, setShowChat] = useState(false);
+	const [roomId, setRoomId] = useState("");
+
+	const sessionUser = useSelector((state) => state.session.user);
+	const userId = request?.user.id;
 
 	useEffect(() => {
 		async function fetchData() {
@@ -17,6 +25,25 @@ const SingleRequest = () => {
 		}
 		fetchData();
 	}, [requestId]);
+
+	useEffect(() => {
+		async function fetchChat() {
+			const res = await fetch(`/api/chats/${sessionUser.id}-${userId}/`);
+			const chat1 = await res.json();
+			const result = await fetch(
+				`/api/chats/${userId}-${sessionUser.id}/`
+			);
+			const chat2 = await result.json();
+			if (chat1) {
+				setRoomId(`${sessionUser.id}-${userId}`);
+			} else if (chat2) {
+				setRoomId(`${userId}-${sessionUser.id}`);
+			} else {
+				setRoomId(`${sessionUser.id}-${userId}`);
+			}
+		}
+		fetchChat();
+	}, [userId, sessionUser]);
 
 	return (
 		<div className="wholeTradePage">
@@ -35,9 +62,7 @@ const SingleRequest = () => {
 							<img
 								alt="profile"
 								className="profilePicSingle"
-								src={
-									request?.user.profilePic
-								}
+								src={request?.user.profilePic}
 							/>
 						</div>
 					</h2>
@@ -52,6 +77,34 @@ const SingleRequest = () => {
 				<p className="singleDistance">
 					{distance ? distance.toFixed() : distance} miles away
 				</p>
+				<p className="singleDistance">
+					Interested?{" "}
+					<button
+						className="backNav"
+						id="messageButton"
+						type="button"
+						onClick={() => setShowChat(true)}
+					>
+						Send {request?.user.username} a Message
+					</button>
+				</p>
+				<ReactModal
+					isOpen={showChat}
+					contentLabel="chat2Modal"
+					className="loginModal"
+				>
+					<Messages
+						setShowChat={setShowChat}
+						roomId={roomId}
+						appElement={Messages}
+					/>
+					<button
+						className="windowCloseButton"
+						onClick={() => setShowChat(false)}
+					>
+						<i className="fas fa-window-close"></i>
+					</button>
+				</ReactModal>
 			</div>
 		</div>
 	);
